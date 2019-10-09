102b5a8fed82db8411c10bafad9cbef9bc7625ef
JakeS <jacobscearcy@gmail.com>
2019-10-08 23:34:15 -0700

    Bug/8/navigation (#12)
    * Issue #8 handle more workspace cases
    
    * properly discover packages in rust workspace

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,7 +1,8 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import { Option } from './option';
-import { readdir, readdirSync, statSync } from 'fs';
+import { readdir, readdirSync, readFile, statSync } from 'fs';
+import { parse } from 'toml';
 import { Utilities, Queue } from './utilities';
 
 export class Configuration {
@@ -15,7 +16,7 @@ export class Configuration {
 
     static async createConfiguration(): Promise<Configuration> {
         if (vscode.workspace.workspaceFolders) {
-            const subDirectories = await this.getRustDirectories(vscode.workspace.workspaceFolders);
+            const [subDirectories, packageNames] = await this.getRustDirectories(vscode.workspace.workspaceFolders);
             let quickPickSelection;
             if (subDirectories && subDirectories.length === 1) {
                 quickPickSelection = subDirectories[0];
@@ -30,8 +31,7 @@ export class Configuration {
             }
 
             if (quickPickSelection) {
-                const [packageName, packagePath] = await this.getCargoPackagePath(quickPickSelection);
-                console.log(packageName, packagePath);
+                const [packageName, packagePath] = await this.getCargoPackagePath(quickPickSelection, packageNames);
                 return Promise.resolve(new Configuration(packageName, packagePath));
             } else {
                 return this.rejectInstantiation('Please select a folder to display');
@@ -52,16 +52,15 @@ export class Configuration {
     getWorkspaceName(): string {
         return this.workspaceName;
     }
-
-    private static async getCargoPackagePath(projectFolderPath: string): Promise<[string, string]> {
+  
+    private static async getCargoPackagePath(projectFolderPath: string, packageNames: string[]): Promise<[string, string]> {
         const packageTargetPath = path.join(projectFolderPath, 'target', 'doc');
         const readDirs = readdirSync(packageTargetPath)
             .reduce((docDirectories: string[], readdirItem) => {
                 const stats = statSync(path.join(packageTargetPath, readdirItem));
                 if (
                     stats.isDirectory() &&
-                    readdirItem !== 'src' &&
-                    readdirItem !== 'implementors'
+                    packageNames.some(item => item === readdirItem)
                 ) {
                     docDirectories.push(readdirItem);
                 }
@@ -92,20 +91,24 @@ export class Configuration {
         return packageName.split('-').join('_');
     }
 
-    private static async getRustDirectories(workspaceFolders: vscode.WorkspaceFolder[]): Promise<string[]> {
+    private static async getRustDirectories(workspaceFolders: vscode.WorkspaceFolder[]): Promise<[string[], string[]]> {
         const rustDirectories = new Set<string>();
+        const rustPackages = new Set<string>();
         for (let i = 0; i < workspaceFolders.length; i++) {
             const workspaceFolder = workspaceFolders[i];
-            const subDirectories = await this.getRustSubDirectories(workspaceFolder);
+            const [subDirectories, packages] = await this.getRustSubDirectories(workspaceFolder);
             subDirectories.forEach(dir => rustDirectories.add(dir));
+            packages.forEach((val) => rustPackages.add(this.formatPackageName(val)));
         }
 
-        return Array.from(rustDirectories);
+        return [Array.from(rustDirectories), Array.from(rustPackages)];
     }
 
-    private static async getRustSubDirectories(workspaceFolder: vscode.WorkspaceFolder): Promise<string[]> {
+    private static async getRustSubDirectories(workspaceFolder: vscode.WorkspaceFolder): Promise<[string[], string[]]> {
         const readDirPromise = Utilities.toPromise(readdir);
-        const rustDirectories = new Set<string>();
+        const readFilePromise = Utilities.toPromise(readFile); 
+        const targetDirectories = new Set<string>();
+        const packageNames = new Set<string>();
         const subDirQueue = new Queue<string>();
         subDirQueue.enqueue(workspaceFolder.uri.fsPath);
 
@@ -113,16 +116,24 @@ export class Configuration {
             const folderToSearch = subDirQueue.dequeue();
             const folderChildren = await readDirPromise<string[]>(folderToSearch).catch(() => [] as string[]);
             if (folderChildren.indexOf('target') >= 0) {
-                rustDirectories.add(folderToSearch);
-            } else {
-                const visibleChildren = folderChildren
-                    .filter(folder => !(/(^|\/)\.[^\/\.]/g).test(folder))
-                    .map(folder => path.join(folderToSearch, folder));
-                subDirQueue.enqueueMany(visibleChildren);
+                targetDirectories.add(folderToSearch);
             }
+            if (folderChildren.indexOf('Cargo.toml') >= 0) {
+                const cargoPath = path.join(folderToSearch, 'Cargo.toml');
+                const cargoFile: string = await readFilePromise(cargoPath, { encoding: 'utf8' });
+                const cargoFileDeserialized = parse(cargoFile);
+                if (cargoFileDeserialized && cargoFileDeserialized.package) {
+                    packageNames.add(cargoFileDeserialized.package.name);
+                }
+            }
+
+            const visibleChildren = folderChildren
+                .filter(folder => !(/(^|\/)\.[^\/\.]/g).test(folder))
+                .map(folder => path.join(folderToSearch, folder));
+            subDirQueue.enqueueMany(visibleChildren);
         }
 
-        return Array.from(rustDirectories);
+        return [Array.from(targetDirectories), Array.from(packageNames)];
     }
 
     private static getPackagePath(projectFolderPath: string, packageName: string): string {
